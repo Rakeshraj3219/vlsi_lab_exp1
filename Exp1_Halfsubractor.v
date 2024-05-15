72c16302882e8313ed63398ae70ae59ed47ea8f0
Rakeshraj3219 <rakeshraj32192003@gmail.com>
2024-05-15 19:06:22 +0530

    Exp1_Halfsubractor.v

--- Exp1_Halfsubractor.v
+++ Exp1_Halfsubractor.v
@@ -1,4 +1,11 @@
 module hs(a,b,difference,borrow);
+
+module half_adder(a,b,sum,carry);
+input a,b;
+output sum,carry; // sum and carry
+or(sum,a,b);
+and(carry,a,b);
+endmodule
 input a,b;
 output difference,borrow;
 
